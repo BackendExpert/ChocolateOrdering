94d41b77f646c2a99443000f80b078b860a15f74
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-05 11:58:40 +0530

    Create Nav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/components/HomePage/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='w-full'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ratione rem, mollitia incidunt placeat doloremque iste totam soluta porro quo perspiciatis quas nihil unde magnam adipisci earum delectus repudiandae eligendi!
+    </div>
+  )
+}
+
+export default Nav
