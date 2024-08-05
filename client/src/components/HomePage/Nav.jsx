c501c464d2f84364484561d9c980923635c54e27
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-05 12:20:33 +0530

    Update Nav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/HomePage/Nav.jsx
+++ client/src/components/HomePage/Nav.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { BsPerson } from 'react-icons/bs'
 
 const Nav = () => {
+    const NavList = [
+        {id: 1, name: "Home", link: ""},
+        {id: 2, name: "Home", link: ""},
+        {id: 3, name: "Home", link: ""},
+        {id: 4, name: "Home", link: ""},
+        {id: 5, name: "Home", link: ""},
+        {id: 6, name: "Home", link: ""},        
+    ]
+
   return (
-    <nav className='fixed top-0 z-50'>
-         <div className='w-full'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ratione rem, mollitia incidunt placeat doloremque iste totam soluta porro quo perspiciatis quas nihil unde magnam adipisci earum delectus repudiandae eligendi!
+    <nav className='fixed top-0 z-50 w-full'>
+         <div className='bg-yellow-800 text-white  my-4 md:mx-8 mx-2 py-4 px-8 rounded-full border-b-4 border-yellow-900 shadow-md'>
+            <div className="md:flex justify-between">
+                <div className="">Chocolets</div>
+                <div className="flex">
+                    {
+                        NavList.map((nav, index) => {
+                            return (
+                                <div key={index} className="mx-4 duration-200 hover:border-b border-white cursor-pointer">
+                                    {nav.name}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className="flex cursor-pointer">
+                    <BsPerson className='h-6 w-full pr-2'/>
+                    <p className="">Login</p>
+                </div>
+            </div>
         </div>
     </nav>
   )
