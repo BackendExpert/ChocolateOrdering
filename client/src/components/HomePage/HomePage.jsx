012d3864dbf4a7c48856fd6cf87b4dbd684c5e6a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-05 13:55:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="">
-        <div className='bg-[url(https://wallpapercave.com/wp/wp3192478.jpg)] bg-center bg-cover w-full min-h-screen'>
-            hi all
+    <div className="bg-yellow-950 min-h-screen py-4">
+        <div className='md:mt-20 mt-24 md:mx-10 mx-2 relative rounded-3xl border-2 border-white border-yellow-800  bg-[url(https://wallpapercave.com/wp/wp3192478.jpg)] bg-center bg-cover min-h-screen'>
+            <div className="pt-16 px-12">
+                <h1 className="text-xl text-yellow-800">Chocolets</h1>
+            </div>
         </div>
-        <div className="">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt blanditiis aperiam ratione! Maxime delectus numquam exercitationem nulla inventore. Quae excepturi repellendus voluptatibus atque labore eos quidem eveniet, ex amet. Ullam.
+        <div className="bg-yellow-950 py-8 -mt-8">
+        </div>
+        <div className="bg-yellow-950 py-4">
+            <div className="md:mx-8 rounded-xl py-4 px-8">
+                <div className="md:grid grid-cols-4 gap-4">
+                    <div className="">
+                        <img src="https://wallpapercave.com/wp/wp3064790.jpg" alt="" className='h-40 w-auto'/>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
