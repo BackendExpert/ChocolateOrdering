c7ef6cec1e22b3fd9fff53269e1f0f5261b38064
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-06 06:59:06 +0530

    Update ProductStore.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Products/ProductStore.jsx
+++ client/src/components/Products/ProductStore.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BgImg from '../../assets/BgImg.jpg'
+import ProductNav from './ProductNav'
 
 const ProductStore = () => {
     const styles = {
@@ -7,13 +8,76 @@ const ProductStore = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
     }
+
+    const ProductData = [
+        {id: 1, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "in-Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 2, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "Out of Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 3, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "in-Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 4, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "Out of Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 5, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "in-Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 6, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "Out of Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 7, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "in-Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 8, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "Out of Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 9, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "in-Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        {id: 10, Image: "https://wallpapercave.com/wp/1Pm7E5e.jpg", Name: "Royel Chocolates", price: "$250.00", Stock: "Out of Stock", Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente, officiis repudiandae id quia fugit minus mollitia doloribus placeat distinctio consequatur quibusdam natus possimus perferendis provident nulla nesciunt harum pariatur."},
+        
+    ]
   return (
-    <div>
-        <div className="fixed min-h-screen w-full left-0 right-0 border-b p-4 z-60 border-yellow-800 bg-cover bg-center" style={styles}>           
+    <div className="bg-cover bg-center min-h-screen py-4 relative ">
+        <div className="-mt-4 fixed min-h-screen w-full left-0 right-0 border-b p-4 z-60 border-yellow-800 bg-cover bg-center" style={styles}>           
         </div>
         <div className="">
+            <div className='z-40 md:mt-16 mt-24 md:mx-10 mx-2 relative rounded-3xl bg-center bg-cover'>
+                <ProductNav />
+            </div>
+            <div className='z-40 md:mt-8 mt-24 md:mx-10 mx-2 relative rounded-3xl bg-center bg-cover'>
+                <div className="md:grid grid-cols-4 gap-4">
+                    {
+                        ProductData.map((product, index) => {
+                            return(
+                                <div className="bg-white py-8 px-4 shadow-md rounded-xl" key={index}>
+                                    <div className="flex justify-center items-center">
+                                        <img src={product.Image} alt="Centered" className="max-w-full h-40" />
+                                    </div>
+            
+                                    <div className="px-4 py-6">
+                                        <h1 className="text-xl text-yellow-800 font-semibold">{product.Name}</h1>
+                                        <p className="py-4">
+                                            {product.Desc}
+                                        </p>
+            
+                                        <div className="flex justify-between">
+                                            <h1 className="text-xl text-yellow-800 font-semibold">{product.price}</h1>
+                                            {
+                                                (() => {
+                                                    if(product.Stock === "Out of Stock"){
+                                                        return (
+                                                            <h1 className="text-xl text-red-700 font-semibold">{product.Stock}</h1>
+                                                        )
+                                                    }
+                                                    else{
+                                                        return (
+                                                            <h1 className="text-xl text-yellow-800 font-semibold">{product.Stock}</h1>
+                                                        )
+                                                    }
+                                                })()
+                                            }
+                                            
+                                        </div>  
+            
+                                        <button className='bg-yellow-800 py-2 px-8 rounded mt-6 w-full text-white'>View More</button>                 
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                   
+                </div>
+            </div>
+
 
         </div>
+
     </div>
   )
 }
