453ef350122b1534cac21d7282d562f95f443162
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-06 06:28:27 +0530

    Update ProductStore.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Products/ProductStore.jsx
+++ client/src/components/Products/ProductStore.jsx
@@ -9,7 +9,7 @@ const ProductStore = () => {
     }
   return (
     <div>
-        <div className="-mt-4 fixed min-h-screen w-full left-0 right-0 border-b p-4 z-60 border-yellow-800 bg-cover bg-center" style={styles}>           
+        <div className="fixed min-h-screen w-full left-0 right-0 border-b p-4 z-60 border-yellow-800 bg-cover bg-center" style={styles}>           
         </div>
         <div className="">
 
