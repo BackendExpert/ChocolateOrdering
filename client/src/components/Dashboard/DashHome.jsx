7d4ed4a96d6f1381047bb8751beb0cde67de686d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-06 20:16:06 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashHome.jsx
+++ client/src/components/Dashboard/DashHome.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import CountUp from 'react-countup'
-import { Bs0Circle, Bs0SquareFill } from 'react-icons/bs'
+import { Bs0Circle, Bs0SquareFill, BsArrowDownUp, BsBoxSeam, BsCurrencyDollar, BsPeople } from 'react-icons/bs'
 
 const DashHome = () => {
     const DashData = [
-      {id: 1, name: "Foods", value: <CountUp end={20} />, icons: <Bs0Circle className="h-12 w-auto"/> },
-      {id: 1, name: "Foods", value: <CountUp end={20} />, icons: <Bs0Circle className="h-12 w-auto"/> },      
-      {id: 1, name: "Foods", value: <CountUp end={20} />, icons: <Bs0Circle className="h-12 w-auto"/> },      
-      {id: 1, name: "Foods", value: <CountUp end={20} />, icons: <Bs0Circle className="h-12 w-auto"/> },
-      
+      {id: 1, name: "Items", value: <CountUp end={20} />, icons: <BsBoxSeam className="h-12 w-auto"/> },
+      {id: 2, name: "Orders", value: <CountUp end={20} />, icons: <BsArrowDownUp className="h-12 w-auto"/> },      
+      {id: 3, name: "Customers", value: <CountUp end={20} />, icons: <BsPeople className="h-12 w-auto"/> },      
     ]
 
   return (
@@ -19,12 +17,17 @@ const DashHome = () => {
           <div className="md:flex">
             <div className="md:w-1/4 text-center">
               <div className="bg-white py-4 px-8 rounded shadow-md mt-4">
-                <center><Bs0SquareFill className='h-36 w-auto text-yellow-800 '/></center>
+                <center><BsCurrencyDollar className='h-36 w-auto text-yellow-800 '/></center>
                 <h1 className="pt-4 pb-1 text-xl font-semibold text-yellow-900">Monthly Income</h1>
                 <h1 className="pt-1 pb-3 text-center text-2xl font-semibold text-yellow-900">$1500.00</h1>
                 <p className=""></p>
               </div>
+              <div className="bg-white my-2 py-4 px-6 rounded shadow-md">
+                <h1 className="text-xl font-semibold text-yellow-800">Trending Chocolets</h1>
+              </div>
             </div>
+
+
             <div className="w-full">
               <div className="md:grid grid-cols-3 gap-2 mt-1">
                 {
@@ -33,8 +36,14 @@ const DashHome = () => {
                       <div className="mt-3 md:pl-4" key={index}>
                         <div className="bg-yellow-800 text-white py-8 rounded shadow-md px-4 w-full">
                           <div className="flex justify-between">
-                            {dash.icons}
-                            <h1 className="text-xl">{dash.name}</h1>
+                            <div className="">
+                            <h1 className="text-3xl">{dash.value}</h1>
+                            <h1 className="text-xl py-2">{dash.name}</h1>
+                            </div>
+                            <div className="mr-4">
+                              {dash.icons}
+                              
+                            </div>
                           </div>
                         </div>
                       </div>
