58946ba5a99c529382d0532c798d06f532f3cc7d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-06 06:24:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Products/ProductStore.jsx
+++ client/src/components/Products/ProductStore.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import BgImg from '../../assets/BgImg.jpg'
 
 const ProductStore = () => {
+    const styles = {
+        background: `url(${BgImg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }
   return (
     <div>
         <div className="-mt-4 fixed min-h-screen w-full left-0 right-0 border-b p-4 z-60 border-yellow-800 bg-cover bg-center" style={styles}>           
         </div>
         <div className="">
-            
+
         </div>
     </div>
   )
