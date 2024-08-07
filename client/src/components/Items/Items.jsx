ed4587f2d9c746772afa3eb984e69d9eb8f8a25b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-07 12:35:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Items/Items.jsx
+++ client/src/components/Items/Items.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ItemsData from './ItemsData'
 
 const Items = () => {
   return (
-    <div>Items</div>
+    <div>
+        <div className="">
+            <h1 className="text-xl font-semibold text-yellow-800">Items</h1>
+            <ItemsData />
+        </div>
+    </div>
   )
 }
 
---
+++ client/src/components/Items/ItemsData.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import CountUp from 'react-countup'
+import { BsBoxFill } from 'react-icons/bs'
+
+const ItemsData = () => {
+    const ItemsNav = [
+        {id: 1, link:"AllItems", value: <CountUp end={20} />, name: "Items", icon: <BsBoxFill className='h-8 w-auto'/>},
+        {id: 2, link:"AllItems", value: <CountUp end={20} />, name: "Items", icon: <BsBoxFill className='h-8 w-auto'/>},
+        {id: 3, link:"AllItems", value: <CountUp end={20} />, name: "Items", icon: <BsBoxFill className='h-8 w-auto'/>},        
+        {id: 3, link:"AllItems", value: "#", name: "Add Items", icon: <BsBoxFill className='h-8 w-auto'/>}, 
+    ]
+  return (
+    <div>
+        <div className="md:grid grid-cols-4 gap-2 my-2">
+            {
+                ItemsNav.map((itmes, index) => {
+                    return (
+                        <div className="bg-yellow-800 rounded shadow-md text-white py-4 px-4" key={index}>
+                            <div className="flex justify-between">
+                                <div className="">
+                                    <p className="text-2xl">{itmes.value}</p>
+                                    <p className="text-xl">{itmes.name}</p>
+                                </div>
+                                <div className="mt-4 mr-4">
+                                    {itmes.icon}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default ItemsData
