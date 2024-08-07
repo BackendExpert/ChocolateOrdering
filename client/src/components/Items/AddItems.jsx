4df170f0ccb6d01fd68c50091b4ddc95fb9b9b8b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-07 14:11:28 +0530

    Update AddItems.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Items/AddItems.jsx
+++ client/src/components/Items/AddItems.jsx
@@ -5,8 +5,12 @@ const AddItems = () => {
   return (
     <div>
         <div className="">
-            <h1 className="text-xl font-semibold text-yellow-800">Items</h1>
+            <h1 className="text-xl font-semibold text-yellow-800">Add New Items</h1>
             <ItemsData />
+
+            <div className="">
+
+            </div>
         </div>
     </div>
   )
