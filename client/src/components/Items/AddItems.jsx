226368b7706a81bf34b06b6c7c1baffb61da66dd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-12 11:40:44 +0530

    Update AddItems.jsx

--- client/src/components/Items/AddItems.jsx
+++ client/src/components/Items/AddItems.jsx
@@ -2,14 +2,36 @@ import React from 'react'
 import ItemsData from './ItemsData'
 
 const AddItems = () => {
+    const headleSubmit = (e) => {
+      e.preventDefault();
+    }
   return (
     <div>
         <div className="">
             <h1 className="text-xl font-semibold text-yellow-800">Add New Item</h1>
             <ItemsData />
 
-            <div className="">
-
+            <div className="bg-white rounded shadow-md py-8 px-4 my-4">
+              <form onSubmit={headleSubmit} method="post">
+                  <div className="md:grid grid-cols-3 gap-4">
+                    <div className="">
+                      <input type="text" name="" id="" className="h-12 w-full bg-gray-200 pl-2 rounded" required placeholder='Item Number'
+                      />
+                    </div>
+                    <div className="">
+                      <input type="text" name="" id="" className="h-12 w-full bg-gray-200 pl-2 rounded" required placeholder='Item Name'
+                      />
+                    </div>
+                    <div className="">
+                        <input type="number" name="" id="" className="h-12 w-full bg-gray-200 pl-2 rounded" required placeholder='Item Price'
+                        />
+                    </div>
+                  </div>
+                  <div className="">
+                      <textarea className='w-full h-24 rounded bg-gray-200 my-4 pl-2' placeholder='Item Description' required></textarea>
+                  </div>
+                  <input type="submit" value="Add Item" className='bg-yellow-800 text-white py-2 px-8 rounded shadow-md'/>
+              </form>
             </div>
         </div>
     </div>
