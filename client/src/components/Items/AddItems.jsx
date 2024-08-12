7e1ed4e710729207c662fdd6db8fdd064a82ffaf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-12 11:55:29 +0530

    Update AddItems.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Items/AddItems.jsx
+++ client/src/components/Items/AddItems.jsx
@@ -2,12 +2,31 @@ import React, { useState } from 'react'
 import ItemsData from './ItemsData'
 
 const AddItems = () => {
-    const [ItemsData, SetItemDate] = useState({
-      
+    const [ItemData, SetItemData] = useState({
+        ItemNumber: '',
+        ItemName: '',
+        ItemPrice: '',
+        ItemDesc: ''
     })
 
-    const headleSubmit = (e) => {
+    const headleSubmit = async (e) => {
       e.preventDefault();
+
+      try{
+        const res = await axios.post(`${import.meta.env.VITE_SERVER_API}/Items/AddItems`, ItemData)
+        .then(res => {
+          if(res.data.Status === "Success"){
+            alert("Item Added Successfully")
+            window.location.reload()
+          }
+          else{
+            alert(res.data.Error)
+          }
+        })
+      }
+      catch (err) {
+        console.log(err)
+      }
     }
   return (
     <div>
@@ -20,19 +39,20 @@ const AddItems = () => {
                   <div className="md:grid grid-cols-3 gap-4">
                     <div className="">
                       <input type="text" name="" id="" className="h-12 w-full bg-gray-200 pl-2 rounded" required placeholder='Item Number'
-                      />
+                      onChange={e => SetItemData({...ItemData, ItemNumber:e.target.value})}/>
                     </div>
                     <div className="">
                       <input type="text" name="" id="" className="h-12 w-full bg-gray-200 pl-2 rounded" required placeholder='Item Name'
-                      />
+                      onChange={e => SetItemData({...ItemData, ItemName:e.target.value})}/>
                     </div>
                     <div className="">
                         <input type="number" name="" id="" className="h-12 w-full bg-gray-200 pl-2 rounded" required placeholder='Item Price'
-                        />
+                      onChange={e => SetItemData({...ItemData, ItemPrice:e.target.value})}/>
                     </div>
                   </div>
                   <div className="">
-                      <textarea className='w-full h-24 rounded bg-gray-200 my-4 pl-2' placeholder='Item Description' required></textarea>
+                      <textarea className='w-full h-24 rounded bg-gray-200 my-4 pl-2' placeholder='Item Description' required
+                      onChange={e =>  SetItemData({...ItemData, ItemDesc:e.target.value})}></textarea>
                   </div>
                   <input type="submit" value="Add Item" className='bg-yellow-800 text-white py-2 px-8 rounded shadow-md'/>
               </form>
