14c876c3516eb4720b5274915725ff6515d12ac4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-12 13:27:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/ItemController.js
+++ server/Controllers/ItemController.js
@@ -1,4 +1,5 @@
 const Item = require("../Models/Item");
+const path = require('path');
 
 const ItemController = {
     AddItems: async (req, res) => {
--- server/Routes/ItemRoute.js
+++ server/Routes/ItemRoute.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const ItemController = require('../Controllers/ItemController');
+const multer = require('multer');
+const path = require('path');
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    },
+});
+
+const upload = multer({ storage });
 
 router.post('/AddItems', ItemController.AddItems)
 
