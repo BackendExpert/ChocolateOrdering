ebffa6a7abcf7dff2d067e31983e822181009621
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-12 15:14:26 +0530

    Update ItemController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/ItemController.js
+++ server/Controllers/ItemController.js
@@ -3,7 +3,23 @@ const path = require('path');
 
 const ItemController = {
     AddItems: async (req, res) => {
+        const {
+            ItemNumber,
+            ItemName,
+            ItemPrice,
+            ItesmDesc
+        } = req.body
 
+        const image = req.file.path
+
+        const checkItems = await Item.findOne({ ItemNumber: ItemNumber})
+
+        if(checkItems){
+            return res.json({ Error: "Item Already Exists" })
+        }
+        else{
+            return res.json({ Status: "Success"})
+        }
     }
 };
 
