7539bd2f2bf4866c50692bf8371ec22dc2366f83
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-12 15:10:37 +0530

    Update ItemController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/ItemController.js
+++ server/Controllers/ItemController.js
@@ -3,41 +3,6 @@ const path = require('path');
 
 const ItemController = {
     AddItems: async (req, res) => {
-        const {
-            ItemNumber,
-            ItemName,
-            ItemPrice,
-            ItemDesc
-        } = req.body
-
-        const Itemimg = req.file.path
-
-        console.log(req.body, Itemimg)
-
-
-        const checkItems = await Item.findOne({ ItemNumber: ItemNumber })
-
-        if(checkItems) {
-            return res.json({ Error: "Item Already Exists"})
-        }
-        else{
-            const NewItems = new Item({
-                ItemNumber: ItemNumber,
-                ItemImage: Itemimg,
-                ItemName: ItemName,
-                ItemPrice: ItemPrice,
-                ItemDesc: ItemDesc,
-            })
-
-            const NewItemsResult = await NewItems.save()
-
-            if(NewItemsResult){
-                return res.json({ Status: "Success" })
-            }
-            else{
-                return res.json({ Error: "Internal Server Error"})
-            }
-        }
 
     }
 };
