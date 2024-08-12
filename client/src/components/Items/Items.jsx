8d02124329e589924619563f090c944ca3cec896
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-12 15:56:43 +0530

    Update Items.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Items/Items.jsx
+++ client/src/components/Items/Items.jsx
@@ -34,15 +34,21 @@ const Items = () => {
                             <div class="flex items-center">Staff Info</div>
                         </div>
                     </div>
-                    <div class="md:grid grid-cols-5 px-4 py-5 text-sm text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
-                          <div class="text-gray-500 dark:text-gray-400">P001</div>
-                          <div class="text-gray-500 dark:text-gray-400">Royal Chocos</div>
-                          <div class="text-gray-500 dark:text-gray-400">$ 100.00</div>
-                          <div class="text-gray-500 dark:text-gray-400">12</div>
-                          <div class="text-gray-500 dark:text-gray-400">   
-                              <p className="text-yellow-800 text-sm font-semibold">View Item</p>                       
+                    {
+                      AllItems.map((items, index) => {
+                        return (
+                          <div key={index} class="md:grid grid-cols-5 px-4 py-5 text-sm text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+                              <div class="text-gray-500 dark:text-gray-400">{items.ItemNumber}</div>
+                              <div class="text-gray-500 dark:text-gray-400">{items.ItemName}</div>
+                              <div class="text-gray-500 dark:text-gray-400">${items.ItemPrice}</div>
+                              <div class="text-gray-500 dark:text-gray-400">{items.Stock}</div>
+                              <div class="text-gray-500 dark:text-gray-400">   
+                                  <p className="text-yellow-800 text-sm font-semibold">View Item</p>                       
+                              </div>
                           </div>
-                      </div>
+                        )
+                      })
+                    }
 
                 </div>
             </div>
