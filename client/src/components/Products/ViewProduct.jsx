b7595e937fdea28950461005e77c5a139131a162
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-15 10:55:44 +0530

    Update ViewProduct.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Products/ViewProduct.jsx
+++ client/src/components/Products/ViewProduct.jsx
@@ -17,6 +17,33 @@ const ViewProduct = () => {
         <div className="md:grid grid-cols-2 gap-4">
             <div className="">
                 <h1 className="text-3xl font-semibold">{GetProduct.ItemName}</h1>
+                <div className="my-4">
+                <img src={`${import.meta.env.VITE_SERVER_API}/${GetProduct.ItemImage}`} alt="Centered" className="w-full h-auto rounded shadow-md" />
+                </div>
+            </div>
+            <div className="md:mt-12 mt-2 ml-2">
+                <p className="">{GetProduct.ItemDesc}</p>
+                <div className="my-8">
+                    <div className="flex justify-between">
+                        <div className="text-2xl font-semibold text-yellow-800">$ {GetProduct.ItemPrice}.00</div>
+                        <div className="">
+                            {
+                                (() => {
+                                    if(GetProduct.Stock === 0){
+                                        return (
+                                            <div className="">Out of Stock</div>
+                                        )
+                                    }
+                                    else{
+                                        return (
+                                            <div className="text-green-500 font-semibold text-2xl">Out of Stock</div>
+                                        )
+                                    }
+                                })()
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
